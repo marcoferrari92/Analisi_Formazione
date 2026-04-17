a434a152d4e51a895b75145073271fc73616a5a3
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-17 23:46:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ if uploaded_file is not None:
                     st.metric("Aziende Target", f"{n_aziende_target}", 
                       delta=f"{(n_aziende_target/n_aziende)*100:.1f}% del totale", delta_color = "normal")
                     st.metric("Budget Medio Target", f"€ {budget_target/n_aziende_target:,.0f}".replace(',', 'X').replace('.', ',').replace('X', '.'))
-                    st.caption("Budget medio per il settore target delle aziende attive in quel settore")
+                    st.caption("Budget medio per il settore target delle aziende attive in quel settore (budget target > 0€)")
 
             # --- 1. PREPARAZIONE COLONNE RAGGRUPPAMENTO ---
         # Usiamo questa lista dinamica per evitare il crash se c'è o meno lo STATO
