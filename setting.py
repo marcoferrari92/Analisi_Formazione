bb720cbd65411e21d315a2f3d80950ad3e0b3ba3
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-17 15:52:58 +0200

    Create setting.py

---
+++ setting.py
@@ -0,0 +1,15 @@
+# settings.py
+
+# Colonne del database RNA su cui effettuare la ricerca delle keyword
+COLONNE_RICERCA = [
+    'RNA_TITOLO_MISURA', 
+    'RNA_DESCRIZIONE_PROGETTO', 
+    'RNA_TITOLO_PROGETTO'
+]
+
+# Keyword predefinite che appariranno all'avvio dell'app
+DEFAULT_KEYWORDS = "formazione, competenze, corso, training, innovazione"
+
+# Eventuali altre impostazioni (es. colori o parametri CSV)
+CSV_SEP = ';'
+CSV_ENCODING = 'utf-8-sig'
