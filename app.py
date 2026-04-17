7c4b41dae8cb662aedb321c6f7d1bc8183cd419c
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-17 23:08:02 +0200

    Update app.py

--- app.py
+++ app.py
@@ -123,12 +123,13 @@ if uploaded_file is not None:
             
         with m2:
             st.metric("Aziende", f"{n_aziende}")
+            st.write("")
             st.metric("Totale Aiuti", f"{n_aiuti_totali}")
             st.markdown("<br>", unsafe_allow_html=True)
             st.metric("Budget Totale", f"€ {budget_totale:,.2f}".replace(',', 'X').replace('.', ',').replace('X', '.'))
 
         with m3:
-            st.metric("Aziende ATTIVE", f"{n_aziende_target}", 
+            st.metric("Aziende ATTIVE (nel settore Target)", f"{n_aziende_target}", 
                       delta=f"{(n_aziende_target/n_aziende)*100:.1f}% del totale", delta_color = "normal")
             st.metric("Aiuti Target", f"{n_aiuti_target}",delta=f"{perc_aiuti_target:.1f}% del totale")
             st.metric("Budget Target",
