7bc4481bc86acacd8b350684f349a3eb8e21c755
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-04-22 11:11:53 +0200

    Update settings.py

--- settings.py
+++ settings.py
@@ -16,7 +16,7 @@ CSV_ENCODING = 'utf-8-sig'
 
 
 GUIDA_TIMELINE = """ Vedi se il settore target è in crescita, in declino o se ha un andamento periodico legato a bandi stagionali"""
-GUIDA_TIMEMAP = """I mesi più intensi indicano quando le aziende ricevono più liquidità. È il momento migliore per proporre nuovi investimenti."""
+GUIDA_TIMEMAP = """I mesi più intensi indicano quando le aziende ricevono più liquidità. Individua i momenti migliori per proporre nuovi investimenti"""
         
 
 GUIDA_PARETO = """
