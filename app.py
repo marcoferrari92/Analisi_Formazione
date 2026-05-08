1fc122783fa301e33bdaa23a729fe4b0a828cceb
marcoferrari92 <74426411+marcoferrari92@users.noreply.github.com>
2026-05-08 14:22:50 +0200

    Update app.py

--- app.py
+++ app.py
@@ -249,7 +249,7 @@ if uploaded_file is not None:
         # --- ANALISI RANKING E PARETO ---
         with st.expander("🏆 Ranking Beneficiari e Analisi di Mercato (Pareto)"):
             st.write("")
-            render_ranking_pareto_analysis(df, guida_pareto=GUIDA_PARETO)
+            pareto_analysis(df, guida_pareto=GUIDA_PARETO)
         st.write("")
         st.write("")
 
